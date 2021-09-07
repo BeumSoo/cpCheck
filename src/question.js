ec4dd73fc8e8e8c40988680ee4e77754fa0252b9
Oh JuYeon <dpffpself@naver.com>
2021-09-07 17:51:13 +0900

    commit comment

--- src/question.js
+++ src/question.js
@@ -21,7 +21,7 @@ export function change_progress(now){
 
 /* 🧡 질문 세팅 */
 export function load_questions(){
-    return fetch('./data/question_test.json')
+    return fetch('./data/question.json')
     .then(res=>res.json())
     .then(data => data.questions);
 }//load_questions
